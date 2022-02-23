195e3ae166b36e688eced50413523b42c519bd11
Brau2003 <ccus.braulio.condor@gmail.com>
2022-02-22 20:22:39 -0500

    R4 Clock JS agregado

--- js/app.js
+++ js/app.js
@@ -80,4 +80,24 @@ window.onscroll = function() {
     ubicacionPrincipal = Desplazamiento_Actual;
 }
 
+// CLOCK
 
+const time = document.getElementById('time');
+const date = document.getElementById('date');
+
+const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+];
+
+const interval = setInterval(() => {
+
+    const local = new Date();
+    
+    let day = local.getDate(),
+        month = local.getMonth(),
+        year = local.getFullYear();
+
+    time.innerHTML = local.toLocaleTimeString();
+    date.innerHTML = `${day} ${monthNames[month]} ${year}`;
+
+}, 1000);
