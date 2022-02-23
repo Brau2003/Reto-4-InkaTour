f0c1ee4be65b340357a22b66be3a13d797737dbb
Brau2003 <ccus.braulio.condor@gmail.com>
2022-02-22 20:21:26 -0500

    R4 Calculadora JS agregado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// NAVBAR
+
 const menu = document.querySelector (".menu");
 const openMenuBtn = document.querySelector (".open-menu");
 const closeMenuBtn = document.querySelector (".close-menu");
@@ -8,3 +10,74 @@ function toggleMenu() {
 
 openMenuBtn.addEventListener("click", toggleMenu)
 closeMenuBtn.addEventListener("click", toggleMenu)
+
+// CALCULADORA DE PRESUPUESTO DE VIAJES
+
+let vacationCalc = document.getElementById('vacationCalc')
+     vacationCalc.addEventListener('submit', calcExpenses)
+
+function getValues () {
+    let destiny = document.getElementById('destiny').value,
+         budget = document.getElementById('budget').value,
+         acomodation = document.getElementById('acomodation').value,
+         transport = document.getElementById('transport').value,
+         food = document.getElementById('food').value;
+    
+        //  console.log(destiny, budget, acomodation, transport, food)
+
+    return {destiny, budget, acomodation, transport, food }
+}
+
+function calcExpenses (e) {
+    e.preventDefault ();
+
+    const {destiny, budget, acomodation, transport, food } = getValues();
+    
+    let expenses = parseInt(acomodation) + parseInt(transport) + parseInt(food)
+    let balance = budget - expenses
+
+    UI(destiny, budget, balance)
+}
+
+function UI(destiny, budget, balance) {
+    let result = document.getElementById('result')
+    let dataPrint = document.createElement('div')
+
+    dataPrint.innerHTML = `
+    <div class="container-data row">
+        <div class="col s4">
+          <h6>${destiny}</h6>
+        </div>
+        <div class="col s4">
+          <h6>${budget}</h6>
+        </div>
+        <div class="col s4">
+          <h6>${balance}</h6>
+        </div>
+      </div>
+    `
+
+    result.appendChild(dataPrint);
+
+    reset ();
+}
+
+function reset() {
+    document.getElementById('vacationCalc').reset()
+}
+
+// EFECTO SCROLL
+
+let ubicacionPrincipal  = window.pageYOffset;
+window.onscroll = function() {
+    let Desplazamiento_Actual = window.pageYOffset;
+    if(ubicacionPrincipal >= Desplazamiento_Actual){
+        document.getElementById('nav').style.top = '0';
+    }
+    else{
+        document.getElementById('nav').style.top = '-80px';
+    }
+    ubicacionPrincipal = Desplazamiento_Actual;
+}
+
+
