e35fd68a5cc21f8e0e0cb63d823aa35f4ade795c
Brau2003 <ccus.braulio.condor@gmail.com>
2022-02-23 21:56:35 -0500

    R4 JS Efecto Scroll

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// NAVBAR
+// MENU HAMBURGUESA
 
 const menu = document.querySelector (".menu");
 const openMenuBtn = document.querySelector (".open-menu");
@@ -72,13 +72,20 @@ let ubicacionPrincipal  = window.pageYOffset;
 window.onscroll = function() {
     let Desplazamiento_Actual = window.pageYOffset;
     if(ubicacionPrincipal >= Desplazamiento_Actual){
-        document.getElementById('nav').style.top = '0';
+        document.getElementById('header').style.top = '0';
     }
     else{
-        document.getElementById('nav').style.top = '-80px';
+        document.getElementById('header').style.top = '-80px';
     }
     ubicacionPrincipal = Desplazamiento_Actual;
 }
+
+// const headerScroll = () => {
+//     const header = document.querySelector('#header')
+//     header.classList.toggle('',window.scrollY > 0)
+// }
+// window.addEventListener('scroll', headerScroll);
+
 
 // CLOCK
 
